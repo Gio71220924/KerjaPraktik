c4d425e56b48266e91af31599c3d0c9696aef3c1
Giovanka Steviano Harry Premono <giovanka.steviano@ti.ukdw.ac.id>
2025-10-07 12:41:49 +0700

    Buat Koneksi DB untuk model SVM

--- scripts/decision-tree/SVM.php
+++ scripts/decision-tree/SVM.php
@@ -1,2 +1,21 @@
 <!-- Kode buat model SVM nya -->
 
+<?php
+//Hubungkan ke Database
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "expertt";
+
+// Buat Koneksi
+$conn = new mysqli($servername, $username, $password, $dbname, 3307);
+// Cek Koneksi
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+    }
+// menangkap user_id yang aktif
+$user_id = $argv[1];
+$case_num = $argv[2];
+$awal = microtime(true);
+
+
