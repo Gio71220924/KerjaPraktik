32c82b8c1e7bbe196d63356790fc3df8fb0b5635
Giovanka Steviano Harry Premono <giovanka.steviano@ti.ukdw.ac.id>
2025-10-07 14:24:14 +0700

    Update View SVM

--- resources/views/admin/menu/SVM.blade.php
+++ resources/views/admin/menu/SVM.blade.php
@@ -1,20 +1,4 @@
 <!-- View  -->
-
-<!-- @extends('layouts.admin')
-
-@section ('content')
-    <div class="container-fluid px-4">
-        <h1 class="mt-4">Support Vector Machine</h1>
-        <br><br>
-        <div class="card mb-4">
-            <div class="card-body">
-                Support Vector Machine (SVM) adalah algoritma pembelajaran mesin yang digunakan untuk klasifikasi dan regresi. SVM bekerja dengan mencari hyperplane optimal yang memisahkan data ke dalam kelas-kelas yang berbeda. Hyperplane ini dipilih sedemikian rupa sehingga jarak antara hyperplane dan titik data terdekat dari masing-masing kelas (disebut margin) adalah maksimal. Dengan demikian, SVM berusaha untuk menciptakan batas keputusan yang paling jelas antara kelas-kelas tersebut.
-            </div>
-        </div>
-    </div>
-@endsection -->
-
-
 @extends('layouts.admin')
 
 @section('content')
